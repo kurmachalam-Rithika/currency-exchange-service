288a17074abe0288d789c55d92e82cc627a51e37
Rithika kurmachalam <rithikakurmachalam@Rithikas-MacBook-Air.local>
2024-08-07 02:54:25 +0530

    currency-exchange-service

---
+++ src/main/resources/datadb.sql
@@ -0,0 +1,13 @@
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10001,'USD','INR',65,'');
+
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10002,'USD','INR',65,'');
+
+
+insert into currency_exchange
+(id,currency_from,currency_to,conversion_multiple,environment)
+values(10003,'USD','INR',65,'');
